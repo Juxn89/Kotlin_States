7046c5ff17e72452ff172f8f86e57a66df0dec0d
= <=>
2020-03-03 14:10:01 -0600

    Ejemplo de onSaveInstanceState y onRestoreInstanceState

--- app/src/main/java/com/example/ejemplociclodevida/MainActivity.kt
+++ app/src/main/java/com/example/ejemplociclodevida/MainActivity.kt
@@ -3,13 +3,38 @@ package com.example.ejemplociclodevida
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var nombre = "Carlos"
+    val NOMBRE = "nombre"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val boton = findViewById<Button>(R.id.boton)
+
+        // Toast.makeText(this, nombre, Toast.LENGTH_LONG).show()
+
+        boton.setOnClickListener {
+            nombre = "Juan"
+
+            Toast.makeText(this, nombre, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString(NOMBRE, nombre)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        nombre = savedInstanceState?.getString(NOMBRE)!!
+        Toast.makeText(this, nombre, Toast.LENGTH_LONG).show()
     }
 }
